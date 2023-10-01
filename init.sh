e8c14920964d7d3e28dcc7b2e344dbece335f780
OptimalKnight <kumarojassvi@gmail.com>
2023-10-01 21:47:59 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@ CONTAINER_PORT=8501
 
 docker run -d -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME:$TAG
 
+echo "Image $IMAGE_NAME:$TAG is successfully running on port $HOST_PORT"
+
 sleep 3
 
 open http://localhost:$HOST_PORT 2>/dev/null
-
